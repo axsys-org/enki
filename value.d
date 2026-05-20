4710a2e544e39e01f14ce3410cf830a33d3ec123
ianchanner <ichanner8@gmail.com>
2026-05-20 14:23:04 -0400

    removed bad files

--- value.d
+++
@@ -1,3 +0,0 @@
-value.o: src/value.c include/enki/value.h include/enki/allocator.h
-include/enki/value.h:
-include/enki/allocator.h:
--- vector.d
+++
@@ -1,3 +0,0 @@
-vector.o: src/vector.c include/enki/vector.h include/enki/allocator.h
-include/enki/vector.h:
-include/enki/allocator.h:
